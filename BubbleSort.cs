c83305edd215e2ff1e7e238770c31cfbf10042d4
zarpluh <rado.zarev@gmail.com>
2020-12-18 15:59:18 +0200

    Create BubbleSort.cs

---
+++ BubbleSort.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+           // Console.WriteLine(arr[arr.Length - 1]);
+        }
+    }
+}
