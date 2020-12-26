7b2e115c6496a46a2314f7c78e7593300484be2c
zarpluh <rado.zarev@gmail.com>
2020-12-26 10:20:06 +0200

    Create SelectionSort.cs

---
+++ SelectionSort.cs
@@ -0,0 +1,25 @@
+using System;
+class SelectionSort
+{
+    static void Main()
+    {
+
+        int[] array = new int[] { 64, 25, 12, 22, 11 };
+        for (int i = 0; i < array.Length – 1; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] > array[j]) // swap items
+                {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < array.Length; i++) // print sorted array
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
