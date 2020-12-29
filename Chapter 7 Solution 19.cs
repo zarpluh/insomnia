cf7e7fb4c77aa7bfd73aed9d0040004e862e05b9
zarpluh <rado.zarev@gmail.com>
2020-12-29 09:51:37 +0200

    Create Chapter 7 Solution 19.cs

---
+++ Chapter 7 Solution 19.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace SieveOfErathosthen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool[] A = new bool[n + 1];
+            for (int i = 0; i <= n; i++)
+            { A[i] = true; }
+            A[0] = false; A[1] = false;
+            for (int i = 0; i < n + 1; i++)
+            {
+                if (A[i])
+                {
+                    for (int j = 2; (j * i) <= n; j++)
+                    { A[j * i] = false; }
+                }
+            }
+            for (int j = 2; j <= n; j++)
+            {
+                if (A[j] == true) { Console.Write(j + " "); }
+            }
+            Console.WriteLine();
+        }
+    }
+}
