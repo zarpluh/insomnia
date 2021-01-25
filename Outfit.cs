2b99e2901b1b575bcca88cb3b86671e799d13312
zarpluh <rado.zarev@gmail.com>
2021-01-25 19:20:02 +0200

    Update Outfit.cs

--- Outfit.cs
+++ Outfit.cs
@@ -1,49 +1,63 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Chapter_9_Solution_10
+class GFG
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        double degrees = double.Parse(Console.ReadLine());
+        string timeOfDay = Console.ReadLine();
+        string outfit = "";
+        string shoes = "";
+        if (degrees >= 10 && degrees <= 18)
         {
-        	double degrees=double.Parse(Console.ReadLine());
-        	string timeOfDay=Console.ReadLine();
-        	string outfit="";
-        	string shoes="";
-        	if(degrees>=10 && degrees<=18)
-        	{
-        		if(timeOfDay=="Morning")
-        		{
-        			outfit="Sweatshirt";
-        			shoes="Sneakers";
-        		}
-        	
-        	else if(timeOfDay=="Aftrnoon" || timeOfDay=="Evening")
-        	{
-        		outfit="Shirt";
-        		shoes="Moccasins";
-        	}
-        	
-        	if(degrees>18 && degrees<=24)
-        	{
-        		if(timeOfDay=="Morning") || (timeOfDay=="Evening")
-        		{
-        			outfit="Shirt";
-        			shoes="Moccasins";
-        		}
-        	
-        	else if(timeOfDay=="Aftrnoon")
-        	{
-        		outfit="T-Shirt";
-        		shoes="Sandals";
-        	}
-        	
-        	
-        	Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+            if (timeOfDay == "Morning")
+            {
+                outfit = "Sweatshirt";
+                shoes = "Sneakers";
+            }
+
+            else if (timeOfDay == "Aftrnoon" || timeOfDay == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+
         }
+
+        if (degrees > 18 && degrees <= 24)
+        {
+            if (timeOfDay == "Afternoon")
+            {
+                outfit = "T-Shirt";
+                shoes = "Sandals";
+            }
+
+            else if (timeOfDay == "Мorning" || timeOfDay == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+        }
+
+        if (degrees >= 25)
+        {
+            if (timeOfDay == "Мorning")
+            {
+                outfit = "T-Shirt";
+                shoes = "Sandals";
+            }
+            if (timeOfDay == "Afternoon")
+            {
+                outfit = "Swim Suit";
+                shoes = "Barefoot";
+            }
+            if (timeOfDay == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+        }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
     }
 }
